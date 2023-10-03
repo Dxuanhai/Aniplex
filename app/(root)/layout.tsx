391cd8f063f194519f56e01a18ee15c640719077
xuanhaibaka <dauhai2005@gmail.com>
2023-10-03 15:51:52 +0700

    #5 update auththenticate feture

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,10 @@
 import "../globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { cookies } from "next/headers";
 import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
+import Authenticate from "@/components/shared/Authenticate";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
@@ -15,12 +17,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const cookieStore = cookies();
+  const isUserLogin = cookieStore.get("userLogin");
+
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
-        <Header />
-        {children}
-        <Footer />
+        <Authenticate type={true}>
+          <Header />
+          {children}
+          <Footer />
+        </Authenticate>
       </body>
     </html>
   );
---
+++ components/shared/Authenticate.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+interface Props {
+  children: React.ReactNode;
+  type: boolean;
+}
+const Authenticate = ({ children, type }: Props) => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const value = localStorage.getItem("userLogin");
+
+    if (type === true) {
+      if (!value) {
+        router.push("/login");
+        return;
+      }
+
+      setLoading(true);
+    } else {
+      if (value) {
+        router.push("/");
+        return;
+      }
+      setLoading(true);
+    }
+  }, [loading]);
+  return (
+    <>
+      {loading ? (
+        <>{children}</>
+      ) : (
+        <main className="w-full h-screen flex items-center justify-center relative">
+          <span className="loading loading-ring w-[10px]  text-pink-100  absolute"></span>
+          <span className="loading loading-ring w-[50px]  text-pink-200 absolute"></span>
+          <span className="loading loading-ring w-[100px]  text-pink-300 absolute"></span>
+          <span className="loading loading-ring w-[200px] text-pink-400 absolute "></span>
+          <span className="loading loading-ring w-[400px] text-pink-500 absolute "></span>
+        </main>
+      )}
+    </>
+  );
+};
+
+export default Authenticate;
