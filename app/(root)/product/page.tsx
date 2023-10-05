cc38c99ab77fbab0bbe1b66d1461b9de603aa413
xuanhaibaka <dauhai2005@gmail.com>
2023-10-06 00:27:27 +0700

    fix delop

--- app/(root)/product/page.tsx
+++ app/(root)/product/page.tsx
@@ -1,18 +1,18 @@
-import ProductForm from "@/components/form/ProductForm";
-import prisma from "../../lib/primasdb";
+// import ProductForm from "@/components/form/ProductForm";
+// import prisma from "../../lib/primasdb";
 
-const page = async () => {
-  const data = await prisma.product.findMany({
-    include: {
-      animes: {
-        select: {
-          type: true,
-        },
-      },
-    },
-  });
+// const page = async () => {
+//   const data = await prisma.product.findMany({
+//     include: {
+//       animes: {
+//         select: {
+//           type: true,
+//         },
+//       },
+//     },
+//   });
 
-  return <div>`{`${data}`}`</div>;
-};
+//   return <div>`{`${data}`}`</div>;
+// };
 
-export default page;
+// export default page;
